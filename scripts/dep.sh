4584501fe306463c8a57802f4852620da40c30ca
Onni Hakala <onni@seravo.fi>
2015-09-05 18:30:05 +0300

    strange but ansible wasn't available in packer build environment

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -2,8 +2,9 @@
 #
 # Setup the the box. This runs as root
 
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible
 apt-get -y update
-
-apt-get -y install curl
+apt-get -y install ansible curl
 
 # You can install anything you need here.
