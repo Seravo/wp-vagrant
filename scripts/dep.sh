f3f02b87e23ac542e8deec93076259a6368ff14f
Onni Hakala <onni@seravo.fi>
2015-09-16 15:18:02 +0300

    Don't use latest ansible from ppa

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -3,7 +3,8 @@
 # Setup the the box. This runs as root
 
 apt-get install software-properties-common ca-certificates
-apt-add-repository ppa:ansible/ansible
+# Don't use latest since it might have problems
+# apt-add-repository ppa:ansible/ansible
 apt-get -y update
 apt-get -y install ansible curl
 
