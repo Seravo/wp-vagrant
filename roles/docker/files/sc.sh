Seravo/wp-vagrant

commit 221c85732ab2933131f6d2e9aadb20c1a855fd33

Fix shell syntax issues detected by Shellcheck

roles/docker/files/sc.sh
@@ -110,8 +110,8 @@ in
         #  vagrant ssh
         #
 
-        SSH_FLAGS="-A -i "/home/vagrant/.ssh/id_rsa_vagrant" -p 2222 -o StrictHostKeyChecking=no "
-        echo "Connecting to local Vagrant environment... (ssh -- $@)"
+        SSH_FLAGS="-A -i '/home/vagrant/.ssh/id_rsa_vagrant' -p 2222 -o StrictHostKeyChecking=no "
+        echo "Connecting to local Vagrant environment... (ssh -- $*)"
 
         # Wait for SSH port to become operational
         while ! ssh ${SSH_FLAGS} -q localhost echo "SSH connection confirmed"
@@ -137,7 +137,7 @@ in
     wait-mounts)
         MPATH="/data/wordpress/config.yml"
 
-        for i in $(seq -s " " 15)
+        for _ in $(seq -s " " 15)
         do
             if [ -e "${MPATH}" ]
             then

commit ce53d56f9340a42c8e2c305e723695d419ea51ac

Fix typo introduced in commit 221c8573
Fixes warnings like:
  Warning: Identity file '/home/vagrant/.ssh/id_rsa_vagrant' not
  accessible: No such file or directory.

roles/docker/files/sc.sh
@@ -110,7 +110,7 @@ in
         #  vagrant ssh
         #
 
-        SSH_FLAGS="-A -i '/home/vagrant/.ssh/id_rsa_vagrant' -p 2222 -o StrictHostKeyChecking=no "
+        SSH_FLAGS="-A -i /home/vagrant/.ssh/id_rsa_vagrant -p 2222 -o StrictHostKeyChecking=no "
         echo "Connecting to local Vagrant environment... (ssh -- $*)"
 
         # Count total wait time to be able to bale out if SSH never seems to get up