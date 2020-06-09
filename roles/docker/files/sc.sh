9dd32bcaaa1bc4493bc1ddaccfcc2c6bc85f5bc5
Otto Kekäläinen <otto@seravo.com>
2020-06-09 10:49:33 +0300

    Refactor Seravo Controller SSH connection handling
    - remove SSH delay that was slowing down all 'vagrant ssh' commands
    - don't force container deletion nor 5 second delay on service start
    - delay container start until Vagrant/Virtualbox has set the new hostname
    - automatically start the Docker container if it wasn't running
    - handle errors correctly in sc that is running with 'set -e'
    - emit exit codes is aborting on error
    - remove excess exit 0 in the name of simplicity and readability
    - add user 'vagrant' to group 'docker' so it can run docker commands
      without sudo
    - add sudo in sc script only where needed
    - include package 'tree' and run it and other commands to automatically
      debug the Vagrant environment in case SSH connection failed completely
    - catch and handle unknown parameters if sc invoked with such

--- roles/docker/files/sc.sh
+++ roles/docker/files/sc.sh
@@ -4,10 +4,6 @@ set -o pipefail
 
 IMAGE_NAME="seravo/wordpress:development"
 CONTAINER_NAME="seravo_wordpress"
-HOSTNAME="$(hostname -s)"
-# The end user will see the hostname of the Docker container in their terminal
-# window title and in the console prompt. Inheriting the hostname from the host
-# is good as Vagrant automatically assigns it the box name.
 
 COMMAND="${1:-autostart}"
 case "${COMMAND}"
@@ -16,14 +12,36 @@ in
         set +e
         while true
         do
-            sudo docker exec -it "${CONTAINER_NAME}" bash && break
+            docker exec -it "${CONTAINER_NAME}" bash && break
             sleep 1
         done
         set -e
     ;;
+    autostart)
+        echo "I: Autostart container if it was not running"
+
+        if docker container top "${CONTAINER_NAME}" > /dev/null
+        then
+            echo "Container is already running, not restarting it"
+            exit 0
+        fi
+        $0 start
+    ;;
     start)
         shift
         echo "I: Starting container"
+
+        while [ "$(hostname -s)" == "seravo-wordpress" ]
+        do
+          echo "Hostname unchanged, waiting for hostnamectl to run..."
+          sleep 1
+        done
+
+        HOSTNAME="$(hostname -s)"
+        # The end user will see the hostname of the Docker container in their terminal
+        # window title and in the console prompt. Inheriting the hostname from the host
+        # is good as Vagrant automatically assigns it the box name.
+
         ID="$(docker run -d \
             --name "${CONTAINER_NAME}" \
             --hostname "${HOSTNAME}" \
@@ -37,10 +55,13 @@ in
             --publish "9000:9000" \
             --volume "/data:/data" \
             --restart "always" \
-            --entrypoint "/sbin/swd_init" \
+            --entrypoint "/sbin/swd_init_debug" \
             --env VAGRANT="$(cat /buildtime)" \
             "$@" \
             "${IMAGE_NAME}")"
+
+        # Keep using swd_init_debug as long as the 'docker logs --since 5s' is
+        # used, otherwise it would be shoing the same output over and over.
 
         # NOTE! If running pure Docker without a VirtualBox wrapper, remember
         # to expose Avahi ports with:
@@ -48,40 +69,35 @@ in
         #   --publish "5353:5353/udp" \
 
         # Update Avahi hostname to match container hostname
-        sed "s/wordpress.local/$HOSTNAME.local/g" -i /etc/avahi/services/http.service
+        # Run with sudo in case sc was not invoked as root
+        sudo sed "s/wordpress.local/$HOSTNAME.local/g" -i /etc/avahi/services/http.service
         # Ensure hostname change takes effect
-        systemctl restart avahi-daemon
+        sudo systemctl restart avahi-daemon
 
         if [ -z "${ID}" ]
         then
             echo "E: Container creation failed!"
             exit 1
         fi
-        exit 0
     ;;
     pull)
         echo "I: Pull latest box from Docker Hub"
         docker pull "${IMAGE_NAME}"
-        exit 0
     ;;
-    remove)
+    remove|rm)
         echo "I: Remove current container"
-        # it's ok for this to fail, eg. if container doesn't exist yet
-        set +e
+        # It's OK for this to fail, eg. if container doesn't exist yet
         docker rm --force "${CONTAINER_NAME}" || true
-        sleep 5
-        set -e
-        exit 0
     ;;
-    autostart)
-        echo "I: Autostart container"
+    restart)
+        echo "I: Restart container"
         if [ -e "/data/wordpress/.seravo-controller-autoupdate" ]
         then
             $0 pull
         fi
         $0 remove
+        sleep 5 # wait a bit before restarting
         $0 start
-        exit 0
     ;;
     shell)
         shift
@@ -102,23 +118,21 @@ in
         # Count total wait time to be able to bale out if SSH never seems to get up
         TIME_PASSED=0
 
-        # Always wait first for 10 seconds that Docker had a chance to start
-        sleep 10
-
         # Wait for SSH port to become operational
         # shellcheck disable=SC2086
         while ! ssh ${SSH_FLAGS} -q localhost echo "SSH connection confirmed"
         do
             echo "Waiting for SSH connection..."
-            # Sleep one second longer than what is the docker logs scope to
-            # ensure there are no duplicate lines
+            # Sleep one second longer (6-5=1) than what is the 'docker logs'
+            # scope to ensure there are no duplicate lines
             sleep 6
             TIME_PASSED=$((TIME_PASSED+6))
 
             # Show events from Docker logs so users can have a hint about what
-            # the bootstrap is doing
-            LOGS="$(docker logs --since 5s "${CONTAINER_NAME}")"
-            if [ -n "$LOGS" ]
+            # the bootstrap is doing. Don't attemd to show anything if 'docker
+            # logs' failed to run. Wrapping this in a if also prevents errors
+            # from stopping the whole sc script.
+            if LOGS="$(docker logs --since 5s "${CONTAINER_NAME}")"
             then
                 echo "Events in past 5 seconds:"
                 echo "$LOGS"
@@ -128,13 +142,15 @@ in
             # just abort and exit
             if [[ "$TIME_PASSED" -gt 180 ]] && [ -z "$LOGS" ]
             then
-                echo "Last 30 events:"
-                docker logs --tail 30 "${CONTAINER_NAME}"
+                # Spit out some debug info and exit with error
+                sc debug
                 echo
                 echo "Development environment did not come online in 3 minutes" >&2
                 echo "Skipped running commands ($*)" >&2
                 echo
-                exit
+                echo "If this repeats, please file an issue at https://github.com/Seravo/WordPress"
+                echo
+                exit 1
             fi
         done
 
@@ -162,14 +178,14 @@ in
     wait-mounts)
         MPATH="/data/wordpress/config.yml"
 
-        for _ in $(seq -s " " 15)
+        for _ in $(seq 30)
         do
             if [ -e "${MPATH}" ]
             then
                 # Vagrant mount successful, exit & allow Docker to launch
                 exit 0
             fi
-            sleep 5
+            sleep 2
         done
 
         echo "ERROR: Mounting /data failed, '${MPATH}' not found!"
@@ -184,5 +200,39 @@ in
         # Give the Docker container 60 seconds to exit gracefully, e.g. dump
         # the database and run other triggers
         docker stop --time=60 "${CONTAINER_NAME}"
+    ;;
+    debug)
+        # Ensure debug completes and don't exit on any errors
+        set +e
+        echo
+        echo "Bootstrap log:"
+        cat /data/log/bootstrap.log
+        echo
+        echo "File share status:"
+        mount | grep /data
+        echo
+        tree -L 3 /data
+        echo
+        echo "Docker status:"
+        # sudo needed to also include last lines from journald in case
+        # sc was no invoked as root
+        sudo systemctl status docker --no-pager -l
+        echo
+        docker ps --all
+        echo
+        docker container top seravo_wordpress
+        echo
+        echo "Last 30 events:"
+        docker logs --tail 30 "${CONTAINER_NAME}"
+        echo
+        echo "Kernel version:"
+        uname -a
+        echo
+        echo "VirtualBox utilities version:"
+        VBoxControl --version
+    ;;
+    *)
+      echo "Unknown argument: $*"
+      exit 1
     ;;
 esac
